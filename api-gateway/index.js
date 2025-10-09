ded2cfc69e2a043908dcf26932684f66e37ce92f
Abhishek <abhi.sankhwar22@gmail.com>
2025-10-09 17:35:30 +0530

    env var added

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -2,18 +2,19 @@ import express from "express"
 import { createProxyMiddleware } from "http-proxy-middleware"
 
 const app = express()
+
 const routes = {
-   	"/api/auth": "http://localhost:8081/auth",
-   	"/api/users": "http://localhost:8081/users",
-   	"/api/msgs": "https://whatsapp-fs-sockets.onrender.com/msgs"_
-}
+   	"/api/auth": process.env.AUTH_BACKEND_URL + "/auth",
+   	"/api/users": process.env.AUTH_BACKEND_URL + "/users",
+   	"/api/msgs": CHAT_BACKEND_URL + "/msgs"
+};
 
 for(const route in routes) {
    const target = routes[route];
    app.use(route, createProxyMiddleware({target, changeOrigin: true}));
 }
 
-const PORT = 8083;
+const PORT = process.env.PORT || 8083;
 
 app.listen(PORT, () => {
    console.log(`api gateway started listening on port : ${PORT}`)
